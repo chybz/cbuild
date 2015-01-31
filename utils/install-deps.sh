d16be4d9edf479bd16caf61e900ed2b5396c1a54
Remy Chibois <chybz@chybz.net>
2015-01-31 14:36:55 +0100

    adding Travis support

---
+++ utils/install-deps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+ME=$(basename $0)
+MYDIR=$(dirname $0)
+MYDIR=$(cd $MYDIR && pwd)
+MYTOPDIR=$(cd $MYDIR/.. && pwd)
+
+sudo apt-get install -y build-essential cmake rsync
+
+wget -O /tmp/cpkg-master.zip https://github.com/chybz/cpkg/archive/master.zip
+cd /tmp
+rm -rf cpkg-master
+unzip cpkg-master.zip
+cd cpkg-master
+./utils/autoinst.sh
